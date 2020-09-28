5ff9eb41d8ec3dbcc112daee92966dfea69680c6
Gabriel <gabrielazevedo492@gmail.com>
2020-09-28 15:23:22 -0300

    Adicionado o sobre

---
+++ finance.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void menuSobre (void);
+
+int main (void) {
+
+    menuSobre();
+    return 0;
+
+}
+
+void menuSobre (void) {
+
+    printf("\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                         ///\n");
+	printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+	printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+	printf("///               Departamento de Computação e Tecnologia                   ///\n");
+	printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+	printf("///          Projeto Sistema de Controle de Financias Domésticas            ///\n");
+	printf("///                Developed by  @gabriel_azevedo3 - Sep, 2020              ///\n");
+	printf("///                        gabrielazevedo492@gmail.com                      ///\n");
+	printf("///                                                                         ///\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                         ///\n");
+	printf("///       = = = =  Sistema de Controle de Finanças Domésticas = = = =       ///\n");
+	printf("///                                                                         ///\n");
+    printf("///  O controle de finanças domésticas é uma das atividades necessárias em  ///\n");
+  	printf("///  praticamente todas as residências, uma vez que para conseguir          ///\n");
+	printf("///  equilibrar o orçamento doméstico e evitar o acúmulo de dívidas, se faz ///\n");
+	printf("///  necessário monitorar entradas e saídas de recursos. Tal atividade pode ///\n");
+	printf("///  demandar tempo e esforço, principalmente se for realizado sem a ajuda  ///\n");
+	printf("///  de um software.                                                        ///\n");
+	printf("///                                                                         ///\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+
+}
