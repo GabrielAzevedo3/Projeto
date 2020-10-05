2947fa59fcf0c243a7ac909e7b60d143457a849e
Gabriel <gabrielazevedo492@gmail.com>
2020-10-05 14:04:26 -0300

    Mudanças no main

--- main.c
+++ main.c
@@ -4,6 +4,19 @@ void menuSobre (void);
 
 int main (void) {
 
+    char opcao;
+    while (opcao != 0) {
+        printf("\n\n");
+        printf(" $ $ $   Menu de Opções   $ $ $ \n\n");
+        printf(" $   1. MENU CLIENTES   $         \n");
+        printf(" $   2. MENU DESPESAS   $         \n");
+        printf(" $   3. RELATÓRIOS      $         \n");
+        printf(" $   4. SOBRE           $         \n");
+        printf(" $   0. SAIR            $         \n");
+        printf("\nEscolha sua opção: ");
+        scanf("%c", &opcao);
+    }
+
     menuSobre();
     return 0;
 
