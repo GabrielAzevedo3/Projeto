4a90d4fa013569bc97fb7407fdb12f6218f0ad7d
Gabriel <gabrielazevedo492@gmail.com>
2020-10-05 12:49:42 -0300

    Adicionado a aba menus, e alteração do nome

---
+++ menus.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+
+void menuSobre (void) {
+
+	printf("\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                         ///\n");
+	printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+	printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+	printf("///               Departamento de Computação e Tecnologia                   ///\n");
+	printf("///                   Disciplina DCT1106 - Programação                      ///\n");
+	printf("///          Projeto Sistema de Controle de Financias Domésticas            ///\n");
+	printf("///                Desenvolvido por Gabriel Azevedo dos Santos              ///\n");
+	printf("///                        gabrielazevedo492@gmail.com                      ///\n");
+	printf("///                             @gabriel_azevedo3                           ///\n");
+	printf("///                                   2020                                  ///\n");
+	printf("///                                                                         ///\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                         ///\n");
+	printf("///       = = = =  Sistema de Controle de Finanças Domésticas = = = =       ///\n");
+	printf("///                                                                         ///\n");
+	printf("///  O controle de finanças domésticas é uma das atividades necessárias em  ///\n");
+  	printf("///  praticamente todas as residências, uma vez que para conseguir          ///\n");
+	printf("///  equilibrar o orçamento doméstico e evitar o acúmulo de dívidas, se faz ///\n");
+	printf("///  necessário monitorar entradas e saídas de recursos. Tal atividade pode ///\n");
+	printf("///  demandar tempo e esforço, principalmente se for realizado sem a ajuda  ///\n");
+	printf("///  de um software.                                                        ///\n");
+	printf("///                                                                         ///\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+
+}
