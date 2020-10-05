17d1bbf286460f404ca5eb1ed4665ff42794635f
Gabriel <gabrielazevedo492@gmail.com>
2020-10-05 15:30:52 -0300

    Adicionado o menus.h

--- main.c
+++ main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-void menuSobre (void);
-
 int main (void) {
 
     char opcao;
     while (opcao != 0) {
         printf("\n\n");
         printf(" $ $ $   Menu de Opções   $ $ $ \n\n");
-        printf(" $   1. MENU CLIENTES   $         \n");
-        printf(" $   2. MENU DESPESAS   $         \n");
-        printf(" $   3. RELATÓRIOS      $         \n");
-        printf(" $   4. SOBRE           $         \n");
-        printf(" $   0. SAIR            $         \n");
+        printf(" $     1. MENU CLIENTES   $       \n");
+        printf(" $     2. MENU DESPESAS   $       \n");
+        printf(" $     3. RELATÓRIOS      $       \n");
+        printf(" $     4. SOBRE           $       \n");
+        printf(" $     0. SAIR            $       \n");
         printf("\nEscolha sua opção: ");
         scanf("%c", &opcao);
     }
