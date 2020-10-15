96a7cac40d6211390357ec5e7cde953b0edd8d49
Gabriel <gabrielazevedo492@gmail.com>
2020-10-15 19:22:37 -0300

    Pequenos ajustes em cadastros.c

--- cadastros.c
+++ cadastros.c
@@ -5,39 +5,39 @@
 
 char cadastraCliente (void) {
     char opcao2;
-    printf("\nVocê entrou no Cadastra Cliente ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Cadastra Cliente\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char listaCliente (void) {
     char opcao2;
-    printf("\nVocê entrou no Lista Cliente ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Lista Cliente\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char buscaCliente (void) {
     char opcao2;
-    printf("\nVocê entrou no Busca Cliente ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Busca Cliente\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 char alteraCliente (void) {
     char opcao2;
-    printf("\nVocê entrou no Altera Cliente ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Altera Cliente\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char deletaCliente (void) {
     char opcao2;
-    printf("\nVocê entrou no Deleta Cliente ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Deleta Cliente\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
@@ -46,39 +46,39 @@ char deletaCliente (void) {
 
 char cadastraDespesa (void) {
     char opcao2;
-    printf("\nVocê entrou no Cadastra Despesa ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Cadastra Despesa\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char listaDespesa (void) {
     char opcao2;
-    printf("\nVocê entrou no Lista Despesa ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Lista Despesa\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char buscaDespesa (void) {
     char opcao2;
-    printf("\nVocê entrou no Busca Despesa ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Busca Despesa\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 char alteraDespesa (void) {
     char opcao2;
-    printf("\nVocê entrou no Altera Despesa ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Altera Despesa\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char deletaDespesa (void) {
     char opcao2;
-    printf("\nVocê entrou no Deleta Despesa ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Deleta Despesa\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
@@ -87,39 +87,39 @@ char deletaDespesa (void) {
 
 char cadastraReceita (void) {
     char opcao2;
-    printf("\nVocê entrou no Cadastra Receita ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Cadastra Receita\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char listaReceita (void) {
     char opcao2;
-    printf("\nVocê entrou no Lista Receita ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Lista Receita\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char buscaReceita (void) {
     char opcao2;
-    printf("\nVocê entrou no Busca Receita ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Busca Receita\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 char alteraReceita (void) {
     char opcao2;
-    printf("\nVocê entrou no Altera Receita ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Altera Receita\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char deletaReceita (void) {
     char opcao2;
-    printf("\nVocê entrou no Deleta Receita ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Deleta Receita\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
@@ -128,40 +128,40 @@ char deletaReceita (void) {
 
 char relatorioDiario (void) {
     char opcao2;
-    printf("\nVocê entrou no Relatorio Diario ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Relatorio Diario\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char relatorioSemanal (void) {
     char opcao2;
-    printf("\nVocê entrou no Relatorio Semanal ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Relatorio Semanal\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char relatorioMensal (void) {
     char opcao2;
-    printf("\nVocê entrou no Relatorio Mensal ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Relatorio Mensal\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char relatorioAnual (void) {
     char opcao2;
-    printf("\nVocê entrou no Relatorio Anual ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Relatorio Anual\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
 
 char escolherPeriodo (void) {
     char opcao2;
-    printf("\nVocê entrou no Escolher Periodo ");
-    printf("\nTecle ENTER para voltar ");
+    printf("\nVocê entrou no Escolher Periodo\n ");
+    printf("\nTecle ENTER para voltar");
     opcao2 = getchar();
     return opcao2;
 }
